c690059f6f943b8c3da2e3bc62ab5dc46ed3ad15
Suleiman Ibrahim <suleimanonnet@gmail.com>
2022-12-07 11:49:29 +0100

    Added more test cases and fix contract bugs

--- test/ENS.js
+++ test/ENS.js
@@ -7,6 +7,14 @@ const NAME = "prince";
 const VERY_SHORT_NAME = "z";
 const VERY_LONG_NAME = "pneumonoultramicroscopicsilicovolcanoconiosis"
 const TLD = "wagmi"
+const _DATA = {
+    ethAddress: "0xethAddress",
+    btcAddress: "bTCadDress",
+    ltcAddress: "0xLtcAddress",
+    bnbAddress: "0xBNBADDRESS",
+    solanaAddress: "0xSoLaNaADdReSs"
+}
+const DATA = ["0xethAddress", "bTCadDress", "0xLtcAddress", "0xBNBADDRESS", "0xSoLaNaADdReSs"]
 
 describe("ENS", function() {
     async function deployFixture() {
@@ -17,7 +25,14 @@ describe("ENS", function() {
         return {ens, deployer, acc1, acc2}
     }
 
-    describe("Constructor", function () {
+    async function updateFixture() {
+        const {ens, deployer, acc1, acc2} = await loadFixture(deployFixture)
+        await ens.connect(acc1).setName(NAME, TLD, {value: ethers.utils.parseEther('0.1')})
+
+        return {ens, deployer, acc1, acc2}
+    }
+
+    describe("constructor", function () {
         it("Should set the correct owner", async function () {
             const {ens, deployer} = await loadFixture(deployFixture);
 
@@ -25,7 +40,7 @@ describe("ENS", function() {
         })
     })
 
-    describe("SetName", function () {
+    describe("setName", function () {
         it("Should revert when supplied with very short name", async function() {
             const {ens, acc1} = await loadFixture(deployFixture);            
 
@@ -44,6 +59,75 @@ describe("ENS", function() {
             const actual  = ens.connect(acc1).setName(NAME, TLD, {value: ethers.utils.parseEther('0.01')})
             await expect(actual).be.revertedWithCustomError(ens, "InvalidFunds").withArgs(ethers.utils.parseEther('0.1'), ethers.utils.parseEther('0.01'))
         })
-        
+        it("Should revert with name already exists when supplied with a name already existing", async function() {
+            const {ens, acc1} = await loadFixture(deployFixture);
+
+            let actual  = ens.connect(acc1).setName(NAME, TLD, {value: ethers.utils.parseEther('0.1')})
+            await expect(actual).to.not.be.reverted;
+
+            // Try to set a name already existing
+            actual = ens.connect(acc1).setName(NAME, TLD, {value: ethers.utils.parseEther('0.1')})
+            await expect(actual).to.be.revertedWithCustomError(ens, "NameAlreadyExists");
+        })
+        it("Should register a name and save to storage", async function() {
+            const {ens, acc1} = await loadFixture(deployFixture)
+            await ens.connect(acc1).setName(NAME, TLD, {value: ethers.utils.parseEther('0.1')})
+            
+            expect(await ens.register(TLD, NAME)).to.be.equal(acc1.address)
+        }) 
+        it("Should add name to names array", async function() {
+            const {ens} = await loadFixture(deployFixture)
+            await ens.setName(NAME, TLD, {value: ethers.utils.parseEther('0.1')})
+
+            expect(await ens.names(0)).to.be.equal(`${NAME}.${TLD}`)
+        })  
+        it("Should increase TLD count", async function() {
+            const {ens} = await loadFixture(deployFixture)
+            await ens.setName(NAME, TLD, {value: ethers.utils.parseEther('0.1')})
+
+            expect(await ens.tldCount(TLD)).to.be.equal("1")
+        })        
+        it("Should emit `RegisterName` event", async function() {
+            const {ens, acc1} = await loadFixture(deployFixture)
+            const actual = await ens.connect(acc1).setName(NAME, TLD, {value: ethers.utils.parseEther('0.1')})
+
+            await expect(actual).to.emit(ens, "RegisterName").withArgs(`${NAME}.${TLD}`, acc1.address)
+        })  
+    })
+
+    describe("setData", function () {
+        it("Should revert when called from an unauthorized user", async function () {
+            const {ens, acc2} = await loadFixture(updateFixture)
+
+            await expect(ens.connect(acc2).setData(NAME, TLD, DATA)).to.be.revertedWithCustomError(ens, "Unauthorized")
+        })
+        it("Should update data correctly", async function () {
+            const {ens, acc1} = await loadFixture(updateFixture)
+            await ens.connect(acc1).setData(NAME, TLD, DATA);
+            const data = await ens.data(`${NAME}.${TLD}`);
+            
+            for (let i = 0; i < data.length; i++) {
+                expect(data[i]).to.equal(DATA[i])
+            }
+        })
+        it("Should emit `updateData` event", async function () {
+            const {ens, acc1} = await loadFixture(updateFixture)
+
+            await expect(ens.connect(acc1).setData(NAME, TLD, DATA)).to.emit(ens, "UpdateData").withArgs(`${NAME}.${TLD}`)
+        })
+    })
+
+    describe("withdraw", function () {
+        it("Should revert if called by unauthorized user", async function () {
+            const {ens, acc1} = await loadFixture(updateFixture);
+
+            await expect(ens.connect(acc1).withdraw()).to.be.revertedWithCustomError(ens, "Unauthorized");
+        })
+
+        it("Should should transfer contract balance to owner", async function () {
+            const {ens, deployer} = await loadFixture(updateFixture);
+            
+            await expect(await ens.connect(deployer).withdraw()).to.changeEtherBalances([ens, deployer], [ethers.BigNumber.from(ethers.utils.parseUnits("-0.1")), ethers.BigNumber.from(ethers.utils.parseUnits("0.1"))])
+        })
     })
 })
