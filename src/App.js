4b2c0ae01510a53bf5260a4c9c564a5bbd1e3294
Suleiman Ibrahim <suleimanonnet@gmail.com>
2022-12-08 01:48:50 +0100

    Add  functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   ChakraProvider,
   theme,
@@ -7,13 +7,16 @@ import {
   Button,
   Input,
   Select,
-  Box
+  Box,
+  Link
 } from '@chakra-ui/react';
 import {FiHelpCircle} from "react-icons/fi"
 import {FaWallet} from "react-icons/fa"
 import {CgProfile} from "react-icons/cg"
 import {AiOutlineFire} from "react-icons/ai"
 import {GrOverview} from "react-icons/gr"
+
+const MUMBAI_CHAIN_ID = "0x13881";
 
 function StyledText({t}) {
   return (
@@ -32,19 +35,107 @@ function StyledText({t}) {
 }
 
 function App() {
+
+  const [account, setAccount] = useState(null)
+  const [chainId, setChainId] = useState(null)
+
+  const switchNetwork = async () => {
+		if (window.ethereum) {
+			try {
+				await window.ethereum.request({
+					method: "wallet_switchEthereumChain",
+					params: [{chainId: "0x13881"}]
+				})
+			} catch(e) {
+				if (e.code === 4902) {
+					try {
+						await window.ethereum.request({
+							method: "wallet_addEthereumChain",
+							params: [
+								{
+									chainId: "0x13881",
+									chainName: "Polygon Mumbai Testnet",
+									rpcUrls: ["https://rpc-mumbai.maticvigil.com/"],
+									nativeCurrency: {
+										name: "Mumbai Matic",
+										symbol: "MATIC",
+										decimals: 18
+									},
+									blockExplorerUrls: ["https://mumbai.polygonscan.com"]
+								},
+							],
+						});
+					} catch(e) {
+						console.log(e)
+					}
+				}
+				console.log(e)
+			}
+		} else {
+			alert("Metamask not installed");
+		}
+	}
+
+  const connectWallet = async () => {
+    try {
+      const {ethereum} = window;
+      if (!ethereum) {
+        alert("Please install Metamask wallet")
+        return;        
+      } else {
+        const accounts = await ethereum.request({method: "eth_requestAccounts"});
+        setAccount(accounts[0]);        
+      }
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  const checkWalletStatus = async () => {
+    const {ethereum} = window;
+    if (!ethereum) {
+      console.log("Metamask not installed")
+      return;
+    } else {
+      const accounts = await ethereum.request({method: "eth_accounts"});
+      if (accounts.length !== 0) setAccount(accounts[0])
+
+      const chainId = await ethereum.request({method: "eth_chainId"});
+      setChainId(chainId)
+      
+      ethereum.on('chainChanged', handleChainChange);
+
+      function handleChainChange(_chainId) {
+        window.location.reload()
+      }
+    }
+  }
+
+  useEffect(() => {
+    checkWalletStatus();
+  }, [])
+
+  // useEffect(() => {
+
+  // })
+
+
   return (
     <ChakraProvider theme={theme}> 
       <Flex direction={"column"} align="center" w="100vw" minH="100vh" position={"relative"}>
-        <Box position="absolute" zIndex={"-10"} w="100vw" h="100vh" bgColor={"springgreen"} borderTopRightRadius="0%" borderBottomRightRadius={"450%"}/>
-        {/* Navigation */}
-        <Flex justify="space-around" align="center" w="100vw" height="120px" p="40px 40px">  
+        <Box position="absolute" zIndex={"-10"} w="100%" h="100vh" bgColor={"springgreen"} borderTopRightRadius="0%" borderBottomRightRadius={"450%"}/>
+        {MUMBAI_CHAIN_ID !== chainId && <Box w="100%" h="50px" bgColor={"orange"} textAlign="center" pt="10px">You are not connected to the right network. In order to continue, please switch to the <Link onClick={switchNetwork} display="inline" textDecor={"underline"}>right network</Link></Box>}
+        {/* Navigation */}        
+        
+        <Flex justify="space-around" align="center" w="100%" height="120px" p="40px 40px">  
           <Flex h="100px" w="100px">
             <Text fontFamily={"Diplomata SC, cursive"} fontSize="50px">UD</Text>
           </Flex>            
           <Flex justify={"space-around"} align="center" w="700px" h="80px">
             <Button leftIcon={<CgProfile/>}>My Domain</Button>
             <Button leftIcon={<FiHelpCircle/>}>Help</Button>
-            <Button leftIcon={<FaWallet/>}>Wallet</Button>
+            {!account && <Button leftIcon={<FaWallet/>} onClick={connectWallet}>Connect</Button>}
+            {account && <Button color={"white"} backgroundColor="green" leftIcon={<FaWallet/>}>{account.slice(0, 5)}...{account.slice(-4)}</Button>}          
           </Flex>
         </Flex>
 
